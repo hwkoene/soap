bc3156bdba2c5148815c44ae8c13030999b76bfc
Hans Koene <hwkoene@gmail.com>
2024-09-08 20:27:55 +0200

    Added GNU GPLv3 license.

--- properties.py
+++ properties.py
@@ -1,4 +1,22 @@
-# Create properties for each field      
+"""
+The DataObject package provides a single-decorator way to persist Python objects.
+It acts like a filesystem variant of an ORM.
+Copyright (C) 2024 Hans Koene
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+    
 from datetime import datetime
 from pathlib import Path
 from typing import get_args, get_origin
--- test.py
+++ test.py
@@ -1,3 +1,22 @@
+"""
+The DataObject package provides a single-decorator way to persist Python objects.
+It acts like a filesystem variant of an ORM.
+Copyright (C) 2024 Hans Koene
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from itertools import chain
 from pathlib import Path
 import unittest
