e5b49be9d6229c7461f7c9745abcbef7fb636944
Hans Koene <hwkoene@gmail.com>
2024-11-02 18:21:59 +0100

    Format using ruff.

--- src/dataobject.py
+++ src/dataobject.py
@@ -43,7 +43,7 @@ class DataObject:
         2. Error handling
         3. Use cases:
             a. Check if network persistence is possible (blocked by Functionality #3)
-            
+
         Functionality
         1. Return list[DataObject] fields as DataObjectList to allow querying
         2. Make DataObjectList operations update the object
@@ -51,50 +51,51 @@ class DataObject:
         4. Add archiving functionality
         5. Integrate with NiceGUI
         6. (Custom) transcoding for more types
-        
+
         Bugs:
         1. Attributes annotated with DataObject subclasses get stored as strings, but not restored.
-        
+
     Notes:
         Class variables with no type hint don't get picked up because they are not in __class__.__annotations__.
         A reference to a DataObject annotated with a string will get resolved when queried.
-        
+
     """
+
     __types: dict[str, __qualname__] = {}
-    __directory: Path = Path.cwd() / 'data'
+    __directory: Path = Path.cwd() / "data"
     __directory.mkdir(exist_ok=True)
-        
+
     class DataObjectList(list):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            
-        def filter(self, func=__builtins__['all'], **kwargs) -> DataObjectList:
+
+        def filter(self, func=__builtins__["all"], **kwargs) -> DataObjectList:
             return DataObject.filter(func=func, objects=self, **kwargs)
-            
-        def exclude(self, func=__builtins__['all'], **kwargs) -> DataObjectList:
+
+        def exclude(self, func=__builtins__["all"], **kwargs) -> DataObjectList:
             return DataObject.exclude(func=func, objects=self, **kwargs)
-        
+
         def sort(self, key, reverse=False) -> DataObjectList:
             return sorted(self, key=key, reverse=reverse)
-        
+
     class DataObjectSet(set):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            
-        def filter(self, func=__builtins__['all'], **kwargs) -> DataObjectSet:
+
+        def filter(self, func=__builtins__["all"], **kwargs) -> DataObjectSet:
             return DataObject.filter(func=func, objects=self, **kwargs)
-            
-        def exclude(self, func=__builtins__['all'], **kwargs) -> DataObjectSet:
+
+        def exclude(self, func=__builtins__["all"], **kwargs) -> DataObjectSet:
             return DataObject.exclude(func=func, objects=self, **kwargs)
-        
+
     @classmethod
-    def get(cls, uuid: UUID): 
+    def get(cls, uuid: UUID):
         for subcls in DataObject.__types.values():
             try:
                 return subcls.get(uuid)
             except StopIteration:
                 print(f"No {subcls.__name__} object found with UUID: {uuid}")
-                
+
     @classmethod
     def filter(cls, func=all, **kwargs) -> DataObjectSet:
         matches = DataObjectSet()
@@ -102,7 +103,7 @@ class DataObject:
             if all(hasattr(subcls, k) for k in kwargs.keys()):
                 matches += subcls.filter(func=func, **kwargs)
         return matches
-    
+
     @classmethod
     def exclude(cls, func=all, **kwargs) -> DataObjectSet:
         matches = DataObjectSet()
@@ -110,25 +111,28 @@ class DataObject:
             if all(hasattr(subcls, k) for k in kwargs.keys()):
                 matches += subcls.exclude(func=func, **kwargs)
         return matches
-    
+
     @classmethod
     def all() -> DataObjectSet:
         instances = DataObjectSet()
         for subcls in DataObject.__types.values():
             instances += subcls.all()
         return instances
-        
+
     @classmethod
     def count(cls) -> int:
         counter = 0
         for subcls in DataObject.__types.values():
             counter += subcls.count()
         return counter
-    
+
     @classmethod
     def export() -> None:
-        raise NotImplementedError("This function should export all dataobjects to their own tab in a .csv file.")
-    
+        raise NotImplementedError(
+            "This function should export all dataobjects to their own tab in a .csv file."
+        )
+
+
 class DataObjectEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, DataObject):
@@ -144,48 +148,49 @@ class DataObjectEncoder(json.JSONEncoder):
 
         return super().default(obj)
 
+
 def dataobject(cls):
     class DataObjectList(list):
         def __init__(self, *args, **kwargs):
             self.__container__ = None
             super().__init__(*args, **kwargs)
-        
-        def filter(self, func=__builtins__['all'], **kwargs) -> DataObjectList:
+
+        def filter(self, func=__builtins__["all"], **kwargs) -> DataObjectList:
             return cls.filter(func=func, objects=self, **kwargs)
-            
-        def exclude(self, func=__builtins__['all'], **kwargs) -> DataObjectList:
+
+        def exclude(self, func=__builtins__["all"], **kwargs) -> DataObjectList:
             return cls.exclude(func=func, objects=self, **kwargs)
-        
+
         def save_after(func):
-            
             @wraps(func)
             def wrapper(self, *args, **kwargs):
                 res = func(self, *args, **kwargs)
                 if self.__container__:
                     self.__container__.save(check=False)
                 return res
+
             return wrapper
-        
+
         @save_after
         def append(self, *args, **kwargs):
             return super().append(*args, **kwargs)
-        
+
         @save_after
         def extend(self, *args, **kwargs):
             return super().extend(*args, **kwargs)
-        
+
         @save_after
         def insert(self, *args, **kwargs):
             return super().insert(*args, **kwargs)
-        
+
         @save_after
         def remove(self, *args, **kwargs):
             return super().remove(*args, **kwargs)
-        
+
         @save_after
         def pop(self, *args, **kwargs):
             return super().pop(*args, **kwargs)
-        
+
         @save_after
         def clear(self, *args, **kwargs):
             return super().clear(*args, **kwargs)
@@ -194,178 +199,238 @@ def dataobject(cls):
         def __init__(self, *args, **kwargs):
             self.__container__ = None
             super().__init__(*args, **kwargs)
-        
-        def filter(self, func=__builtins__['all'], **kwargs) -> DataObjectSet:
+
+        def filter(self, func=__builtins__["all"], **kwargs) -> DataObjectSet:
             return cls.filter(func=func, objects=self, **kwargs)
-            
-        def exclude(self, func=__builtins__['all'], **kwargs) -> DataObjectSet:
+
+        def exclude(self, func=__builtins__["all"], **kwargs) -> DataObjectSet:
             return cls.exclude(func=func, objects=self, **kwargs)
-        
+
         def sample(self, k) -> DataObjectList:
             return DataObjectList(random.sample(list(self), k))
-        
+
         def save_after(func):
-            
             @wraps(func)
             def wrapper(self, *args, **kwargs):
                 res = func(self, *args, **kwargs)
                 if self.__container__:
                     self.__container__.save(check=False)
                 return res
+
             return wrapper
-        
+
         @save_after
         def add(self, *args, **kwargs):
             return super().add(*args, **kwargs)
-        
+
         @save_after
         def remove(self, *args, **kwargs):
             return super().remove(*args, **kwargs)
-        
+
         @save_after
         def pop(self, *args, **kwargs):
             return super().pop(*args, **kwargs)
-        
+
         @save_after
         def clear(self, *args, **kwargs):
             return super().clear(*args, **kwargs)
-        
+
     # Keep track of class instances
-    setattr(cls, '__DataObject_instances', DataObjectSet())
-    setattr(cls, '__DataObject_directory', DataObject._DataObject__directory / cls.__name__)
-   
-    # Set object methods 
+    setattr(cls, "__DataObject_instances", DataObjectSet())
+    setattr(
+        cls, "__DataObject_directory", DataObject._DataObject__directory / cls.__name__
+    )
+
+    # Set object methods
     def date_created(self):
         return datetime.fromtimestamp(self.PATH.stat().st_ctime)
-    setattr(cls, 'date_created', date_created)
-    
+
+    setattr(cls, "date_created", date_created)
+
     def delete(self):
-        setattr(self, '__DataObject_deleted', True)
-        setattr(self, '__DataObject_uuid', None)
+        setattr(self, "__DataObject_deleted", True)
+        setattr(self, "__DataObject_uuid", None)
         self.__class__.__DataObject_instances.remove(self)
         self.PATH.unlink()
-    setattr(cls, 'delete', delete)
-        
+
+    setattr(cls, "delete", delete)
+
     # Save the object
     def save(self, check=True):
-        if not getattr(self, '__DataObject_saving'):
+        if not getattr(self, "__DataObject_saving"):
             logging.warning("Saving is disabled for this object!")
             return
-        
+
         if check:
             for field_name in self.__DataObject_fields.keys():
                 getattr(self, field_name)
-            
-        with self.__DataObject_path.open('w', encoding='utf-8') as file:
-            json.dump(self.__DataObject_fields, file, indent='\t', cls=DataObjectEncoder)
-            
-    setattr(cls, 'save', save)
-        
+
+        with self.__DataObject_path.open("w", encoding="utf-8") as file:
+            json.dump(
+                self.__DataObject_fields, file, indent="\t", cls=DataObjectEncoder
+            )
+
+    setattr(cls, "save", save)
+
     # Generate __init__ function for the class based on class_annotations
     original_init = cls.__init__
     class_annotations = inspect.get_annotations(cls)
+
     def __init__(self, **kwargs):
         # Set standard attributes and create file if necessary
-        getattr(cls, '__DataObject_instances').add(self)
-        setattr(self, '__DataObject_saving', True)
-        setattr(self, '__DataObject_fields', {})
-        setattr(self, '__DataObject_uuid', kwargs.get('uuid', uuid4()))
-        setattr(self, '__DataObject_deleted', False)
-        kwargs.pop('uuid', None)
-        setattr(self, '__DataObject_path', getattr(self, '__DataObject_directory') / str(getattr(self, '__DataObject_uuid')))
-        getattr(self, '__DataObject_path').touch(exist_ok=True)
-        setattr(cls, '__str__', lambda x: pformat(getattr(self, '__DataObject_fields')))
+        getattr(cls, "__DataObject_instances").add(self)
+        setattr(self, "__DataObject_saving", True)
+        setattr(self, "__DataObject_fields", {})
+        setattr(self, "__DataObject_uuid", kwargs.get("uuid", uuid4()))
+        setattr(self, "__DataObject_deleted", False)
+        kwargs.pop("uuid", None)
+        setattr(
+            self,
+            "__DataObject_path",
+            getattr(self, "__DataObject_directory")
+            / str(getattr(self, "__DataObject_uuid")),
+        )
+        getattr(self, "__DataObject_path").touch(exist_ok=True)
+        setattr(cls, "__str__", lambda x: pformat(getattr(self, "__DataObject_fields")))
         # TODO: UUID and PATH should be properties without a settter
-        setattr(self, 'UUID', getattr(self, '__DataObject_uuid'))
-        setattr(self, 'PATH', getattr(self, '__DataObject_path'))
-        
+        setattr(self, "UUID", getattr(self, "__DataObject_uuid"))
+        setattr(self, "PATH", getattr(self, "__DataObject_path"))
+
         # Read class variable annotations and defautls to determine arguments
-        defaults = {k:v.default for (k, v) in signature(self.__class__.__init__).parameters.items() if k not in kwargs.keys() and v.default is not Parameter.empty}
+        defaults = {
+            k: v.default
+            for (k, v) in signature(self.__class__.__init__).parameters.items()
+            if k not in kwargs.keys() and v.default is not Parameter.empty
+        }
         kwargs.update(defaults)
         missing_args = set(class_annotations.keys()) - set(kwargs.keys())
         extra_args = set(kwargs.keys()) - set(class_annotations.keys())
-        
+
         if missing_args:
-            raise TypeError(f"{cls.__name__}.__init__() missing arguments {missing_args}")
+            raise TypeError(
+                f"{cls.__name__}.__init__() missing arguments {missing_args}"
+            )
         elif extra_args:
-            raise TypeError(f"{cls.__name__}.__init__() got unexpected keyword arguments {extra_args}")
+            raise TypeError(
+                f"{cls.__name__}.__init__() got unexpected keyword arguments {extra_args}"
+            )
         else:
-            setattr(self, '__DataObject_fields', kwargs)
+            setattr(self, "__DataObject_fields", kwargs)
 
         self.save()
-        
+
         original_init(self)
-    
+
     @wraps(original_init)
     def new_init(self, **kwargs):
         __init__(self, **kwargs)
-    
+
     cls.__init__ = new_init
-    
+
     sig = signature(__init__)
-    params = [Parameter('self', Parameter.POSITIONAL_OR_KEYWORD, annotation=cls)]
-    params += [Parameter(name=name, 
-                        kind=Parameter.POSITIONAL_OR_KEYWORD,
-                        default=getattr(cls, name) if hasattr(cls, name) else Parameter.empty,
-                        annotation=annotation) 
-                for name, annotation in class_annotations.items()]
+    params = [Parameter("self", Parameter.POSITIONAL_OR_KEYWORD, annotation=cls)]
+    params += [
+        Parameter(
+            name=name,
+            kind=Parameter.POSITIONAL_OR_KEYWORD,
+            default=getattr(cls, name) if hasattr(cls, name) else Parameter.empty,
+            annotation=annotation,
+        )
+        for name, annotation in class_annotations.items()
+    ]
     sig = sig.replace(parameters=params, return_annotation=cls)
     __init__.__signature__ = sig
-    setattr(cls, '__init__', __init__)
-    
-    # Make the class a DataObject type    
+    setattr(cls, "__init__", __init__)
+
+    # Make the class a DataObject type
     cls = type(cls.__name__, (DataObject, cls), {})
     DataObject._DataObject__types[cls.__name__] = cls
-    
+
     for field_name, field_type in class_annotations.items():
-        setattr(cls, field_name, make_property(cls, field_name, field_type, DataObjectList, DataObjectSet))
-    
+        setattr(
+            cls,
+            field_name,
+            make_property(cls, field_name, field_type, DataObjectList, DataObjectSet),
+        )
+
     # Define classmethods
-    def check(obj, func, invert, values, callables):        
-        if func(chain(((getattr(obj, k) == v) for k, v in values.items()), 
-                      (v(getattr(obj, k)) for k, v in callables.items()))):
+    def check(obj, func, invert, values, callables):
+        if func(
+            chain(
+                ((getattr(obj, k) == v) for k, v in values.items()),
+                (v(getattr(obj, k)) for k, v in callables.items()),
+            )
+        ):
             return not invert
         else:
             return invert
-    
+
     @classmethod
     def get(cls, uuid: UUID) -> DataObject:
-        return next((obj for obj in cls.__DataObject_instances if obj.UUID == (UUID(uuid) if type(uuid) is str else uuid)), None)
-    setattr(cls, 'get', get)
-        
+        return next(
+            (
+                obj
+                for obj in cls.__DataObject_instances
+                if obj.UUID == (UUID(uuid) if type(uuid) is str else uuid)
+            ),
+            None,
+        )
+
+    setattr(cls, "get", get)
+
     @classmethod
-    def filter(cls, func=__builtins__['all'], objects=cls.__DataObject_instances, **kwargs) -> DataObjectSet:
+    def filter(
+        cls, func=__builtins__["all"], objects=cls.__DataObject_instances, **kwargs
+    ) -> DataObjectSet:
         values = {k: v for k, v in kwargs.items() if not callable(v)}
         callables = {k: v for k, v in kwargs.items() if callable(v)}
-        return DataObjectSet({obj for obj in objects if check(obj, func, invert=False, values=values, callables=callables)})
-    setattr(cls, 'filter', filter)
-    
+        return DataObjectSet(
+            {
+                obj
+                for obj in objects
+                if check(obj, func, invert=False, values=values, callables=callables)
+            }
+        )
+
+    setattr(cls, "filter", filter)
+
     @classmethod
-    def exclude(cls, func=__builtins__['all'], objects=cls.__DataObject_instances, **kwargs) -> DataObjectSet:
+    def exclude(
+        cls, func=__builtins__["all"], objects=cls.__DataObject_instances, **kwargs
+    ) -> DataObjectSet:
         values = {k: v for k, v in kwargs.items() if not callable(v)}
         callables = {k: v for k, v in kwargs.items() if callable(v)}
-        return DataObjectSet({obj for obj in objects if check(obj, func, invert=True, values=values, callables=callables)})
-    setattr(cls, 'exclude', exclude)
-    
+        return DataObjectSet(
+            {
+                obj
+                for obj in objects
+                if check(obj, func, invert=True, values=values, callables=callables)
+            }
+        )
+
+    setattr(cls, "exclude", exclude)
+
     @classmethod
     def all(cls) -> DataObjectSet:
         # return DataObjectSet(cls.__DataObject_instances)
         return cls.__DataObject_instances
-    setattr(cls, 'all', all)
-    
+
+    setattr(cls, "all", all)
+
     @classmethod
     def count(cls) -> int:
         return len(cls.__DataObject_instances)
-    setattr(cls, 'count', count)
-    
+
+    setattr(cls, "count", count)
+
     # Create a folder to store data objects of this type and load instances
     if cls.__DataObject_directory.exists():
         for file in cls.__DataObject_directory.iterdir():
-            with file.open('r', encoding='utf-8') as fp:
+            with file.open("r", encoding="utf-8") as fp:
                 data = json.load(fp)
                 if isinstance(data, dict):
                     cls(**data, uuid=UUID(file.stem))
     else:
         cls.__DataObject_directory.mkdir()
-        
+
     return cls
--- src/properties.py
+++ src/properties.py
@@ -16,14 +16,20 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-    
+
 from datetime import datetime
 from pathlib import Path
 from typing import get_args, get_origin
 from uuid import UUID
 
 
-def make_property(cls, field_name: str, field_type: str | type, data_object_list_class: type, data_object_set_class: type):
+def make_property(
+    cls,
+    field_name: str,
+    field_type: str | type,
+    data_object_list_class: type,
+    data_object_set_class: type,
+):
     """Use the annotations to get the value of the field.
 
     Args:
@@ -32,20 +38,20 @@ def make_property(cls, field_name: str, field_type: str | type, data_object_list
 
     Returns:
         Property: A property object to get and set the field value.
-        
-        
+
+
     """
-            
+
     def default_setter(self, value):
         self.__DataObject_fields[field_name] = value
         self.save()
-        
+
     def default_getter(self):
         return self.__DataObject_fields[field_name]
-    
+
     def list_setter(self, value):
         self.__DataObject_fields[field_name] = data_object_list_class(value)
-        setattr(self.__DataObject_fields[field_name], '__container__', self)
+        setattr(self.__DataObject_fields[field_name], "__container__", self)
         self.save()
 
     def list_getter(self):
@@ -57,28 +63,33 @@ def make_property(cls, field_name: str, field_type: str | type, data_object_list
             obj_get = value_type.get
         else:
             obj_get = None
-        
+
         if obj_get:
-            if __builtins__['all'](isinstance(obj, (str, UUID)) for obj in self.__DataObject_fields[field_name]):
-                self.__DataObject_fields[field_name] = data_object_list_class([obj_get(item) for item in self.__DataObject_fields[field_name]])
-                setattr(self.__DataObject_fields[field_name], '__container__', self)
-                   
-            outdated = False       
+            if __builtins__["all"](
+                isinstance(obj, (str, UUID))
+                for obj in self.__DataObject_fields[field_name]
+            ):
+                self.__DataObject_fields[field_name] = data_object_list_class(
+                    [obj_get(item) for item in self.__DataObject_fields[field_name]]
+                )
+                setattr(self.__DataObject_fields[field_name], "__container__", self)
+
+            outdated = False
             for item in self.__DataObject_fields[field_name]:
-                if item is None or getattr(item, '__DataObject_deleted'):
+                if item is None or getattr(item, "__DataObject_deleted"):
                     outdated = True
                     self.__DataObject_fields[field_name].remove(item)
-            
+
             if outdated:
                 self.save(check=False)
-            
+
         return self.__DataObject_fields[field_name]
-    
+
     def set_setter(self, value):
         self.__DataObject_fields[field_name] = data_object_set_class(value)
-        setattr(self.__DataObject_fields[field_name], '__container__', self)
+        setattr(self.__DataObject_fields[field_name], "__container__", self)
         self.save()
-    
+
     # TODO: Generalise and allow for custom types
     def set_getter(self):
         # Determine if the list contains referces to other dataobjects (list[DataObject] or list['DataObject'])
@@ -89,93 +100,107 @@ def make_property(cls, field_name: str, field_type: str | type, data_object_list
             obj_get = value_type.get
         else:
             obj_get = None
-        
+
         if obj_get:
-            if __builtins__['all'](isinstance(obj, (str, UUID)) for obj in self.__DataObject_fields[field_name]):
-                self.__DataObject_fields[field_name] = data_object_set_class({obj_get(item) for item in self.__DataObject_fields[field_name]})
-                setattr(self.__DataObject_fields[field_name], '__container__', self)
-                   
-            outdated = False       
+            if __builtins__["all"](
+                isinstance(obj, (str, UUID))
+                for obj in self.__DataObject_fields[field_name]
+            ):
+                self.__DataObject_fields[field_name] = data_object_set_class(
+                    {obj_get(item) for item in self.__DataObject_fields[field_name]}
+                )
+                setattr(self.__DataObject_fields[field_name], "__container__", self)
+
+            outdated = False
             for item in self.__DataObject_fields[field_name]:
-                if item is None or getattr(item, '__DataObject_deleted'):
+                if item is None or getattr(item, "__DataObject_deleted"):
                     outdated = True
                     self.__DataObject_fields[field_name].remove(item)
-            
+
             if outdated:
                 self.save(check=False)
-            
+
         return self.__DataObject_fields[field_name]
-    
+
     # TODO: Generalise and allow for custom types
     def datetime_getter(self):
         value_type = type(self.__DataObject_fields[field_name])
         if value_type is float:
-            self.__DataObject_fields[field_name] = datetime.fromtimestamp(self.__DataObject_fields[field_name])
+            self.__DataObject_fields[field_name] = datetime.fromtimestamp(
+                self.__DataObject_fields[field_name]
+            )
         return self.__DataObject_fields[field_name]
-    
+
     def path_getter(self):
         value_type = type(self.__DataObject_fields[field_name])
         if value_type is str:
-            self.__DataObject_fields[field_name] = Path(self.__DataObject_fields[field_name])
+            self.__DataObject_fields[field_name] = Path(
+                self.__DataObject_fields[field_name]
+            )
         return self.__DataObject_fields[field_name]
-        
+
     def dataobject_getter(self):
         if isinstance(self.__DataObject_fields[field_name], (str, UUID)):
-            self.__DataObject_fields[field_name] = field_type.get(self.__DataObject_fields[field_name])
-    
-        if self.__DataObject_fields[field_name] is not None and self.__DataObject_fields[field_name].__DataObject_deleted:
+            self.__DataObject_fields[field_name] = field_type.get(
+                self.__DataObject_fields[field_name]
+            )
+
+        if (
+            self.__DataObject_fields[field_name] is not None
+            and self.__DataObject_fields[field_name].__DataObject_deleted
+        ):
             self.__DataObject_fields[field_name] = None
             self.save(check=False)
             return None
-                
+
         return self.__DataObject_fields[field_name]
-    
+
     def str_getter(self):
         field_value = self.__DataObject_fields[field_name]
         if field_type in self._DataObject__types.keys():
-            if isinstance(field_value, (str, UUID)) :
+            if isinstance(field_value, (str, UUID)):
                 field_value = cls._DataObject__types[field_type].get(field_value)
-            
+
             # if field_value is not None and field_value.__DataObject_deleted:
             if field_value is not None and field_value.__DataObject_deleted:
                 self.__DataObject_fields[field_name] = None
                 self.save(check=False)
                 return None
-        
+
         return field_value
-    
+
     if isinstance(field_type, type):
         # Custom fields
         if field_type is datetime:
             return property(fget=datetime_getter, fset=default_setter)
-            
+
         elif field_type is Path:
             return property(fget=path_getter, fset=default_setter)
-        
+
         # Generic alias iterable fields
         elif get_origin(field_type) is list:
             return property(fget=list_getter, fset=list_setter)
-        
+
         # Generic alias iterable fields
         elif get_origin(field_type) is set:
             return property(fget=set_getter, fset=set_setter)
-            
+
         # DataObject
         elif field_type.__name__ in cls._DataObject__types.keys():
             return property(fget=dataobject_getter, fset=default_setter)
-            
+
         # Resolve regular fields
         else:
             return property(fget=default_getter, fset=default_setter)
-            
+
     # Generic alias iterable fields (Python 3.12)
     elif get_origin(field_type) is list:
         return property(fget=list_getter, fset=list_setter)
-    
+
     # Generic alias iterable fields (Python 3.12)
     elif get_origin(field_type) is set:
-            return property(fget=set_getter, fset=set_setter)
-    
+        return property(fget=set_getter, fset=set_setter)
+
     # 'DataObject'
     elif isinstance(field_type, str):
         return property(fget=str_getter, fset=default_setter)
